e996d9e4d742e89af1ec5e369978a24c26cebd61
Dan Rogers <dan.rogers@shiptheory.com>
2025-12-02 12:05:22 +0000

    Update example PHP script

--- examples/test.php
+++ examples/test.php
@@ -2,12 +2,14 @@
 
 declare(strict_types=1);
 
-sleep(rand(0, 2));
+$sleep_time = rand(0, 5);
+sleep($sleep_time);
+
 $failure = rand(0, 1);
 if ($failure) {
-    echo "Failure";
+    echo "Failure. I slept for $sleep_time seconds";
 } else {
-    echo "The time is " . time();
+    echo "The time is " . date('H:i:s', time()) . ". I slept for $sleep_time seconds";
 }
 
 exit($failure);
